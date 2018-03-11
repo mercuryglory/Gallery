3d8db8488e4ff6c4929c695521f09b2fb0016138
mercuryglory <wzhseu@163.com>
2018-03-12 00:09:37 +0800

    using imageview to replace checkbox resloving padding no effect

--- app/src/main/java/com/mercury/gallery/ImageAdapter.java
+++ app/src/main/java/com/mercury/gallery/ImageAdapter.java
@@ -5,13 +5,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder>{
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
 
     private List<Image>    mData;
     public  LayoutInflater mInflater;
@@ -32,25 +31,25 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder>{
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, int position) {
         final Image image = mData.get(position);
         String path = image.getPath();
 
-        int itemWidth = ScreenUtils.getScreenWidth(mContext)/4;
-        int itemHeight = ScreenUtils.getScreenHeight(mContext)/4;
-//        Glide.with(mContext).load(path).placeholder(new ColorDrawable(Color.parseColor("#E9EBF0")
-//        )).into(holder.ivPhoto);
+        int itemWidth = ScreenUtils.getScreenWidth(mContext) / 4;
+        int itemHeight = ScreenUtils.getScreenHeight(mContext) / 4;
+        //        Glide.with(mContext).load(path).placeholder(new ColorDrawable(Color.parseColor
+        // ("#E9EBF0")
+        //        )).into(holder.ivPhoto);
         ImageLoader.getInstance().loadImage(holder.ivPhoto, path,
                 itemWidth, itemHeight);
         holder.cbSelect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (((CheckBox) v).isChecked()) {
-                    image.setChecked(((CheckBox) v).isChecked());
-                }
+                image.setChecked(!v.isSelected());
+                holder.cbSelect.setSelected(image.isChecked());
             }
         });
-        holder.cbSelect.setChecked(image.isChecked());
+        holder.cbSelect.setSelected(image.isChecked());
 
     }
 
@@ -59,7 +58,7 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder>{
         return mData.size();
     }
 
-    public void setData(List<Image> list){
+    public void setData(List<Image> list) {
         if (list != null) {
             mData.clear();
             mData.addAll(list);
@@ -70,21 +69,12 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder>{
     static class ViewHolder extends RecyclerView.ViewHolder {
 
         ImageView ivPhoto;
-        CheckBox cbSelect;
+        ImageView  cbSelect;
 
         private ViewHolder(View itemView) {
             super(itemView);
             ivPhoto = itemView.findViewById(R.id.iv_photo);
             cbSelect = itemView.findViewById(R.id.cb_select);
-        }
-    }
-
-
-    static class OnScrollListener extends RecyclerView.OnScrollListener {
-
-        @Override
-        public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-
         }
     }
 
